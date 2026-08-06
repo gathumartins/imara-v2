e4e4194c330c7cc5e761a504d4c61ecc08219ad9
gathumartins <gathumartins@gmail.com>
2026-08-06 22:46:54 +0300

    updated the about us page hero title

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,17 +5,130 @@ import { IntroSection } from "@/components/sections/about/intro-section"
 import { MissionValuesSection } from "@/components/sections/about/mission-values-section"
 import { ProgrammeComponentsSection } from "@/components/sections/about/programme-components-section"
 
-export default function AboutPage() {
+export default async function AboutPage() {
+  const query = `
+  {
+  page: page(id: "cG9zdDoxMg==") {
+    id
+    title
+    uri
+    slug
+    link
+    pageBanners {
+      newPageTitle
+      bannerImage {
+        node {
+          mediaItemUrl
+        }
+      }
+    }
+    content
+    featuredImage {
+      node {
+        altText
+        mediaDetails {
+          height
+          width
+        }
+        sourceUrl
+      }
+    }
+    aboutfields {
+      coreStatements {
+        defaultvalue
+        statements {
+          title
+          content
+          shortname
+          icon {
+            node {
+              altText
+              mediaDetails {
+                width
+                height
+              }
+              sourceUrl
+            }
+          }
+        }
+      }
+    }
+  }
+  programs:programs {
+    edges {
+      node {
+        title
+        slug
+        content
+        excerpt
+        uri
+        programfields {
+          icon {
+            node {
+              altText
+              mediaDetails {
+                width
+                height
+              }
+              sourceUrl
+            }
+          }
+        }
+      }
+    }
+  }
+  teams:teams {
+    edges {
+      node {
+        title
+        content
+        featuredImage {
+          node {
+            altText
+            mediaDetails {
+              height
+              width
+            }
+            sourceUrl
+          }
+        }
+        teamfields {
+          designation
+        }
+      }
+    }
+  }
+  headings:heading(id: "cG9zdDozODM=", idType: ID) {
+    siteheadings {
+      headings {
+        program {
+          title
+          subtitle
+        }
+        testimonials {
+          title
+          subtitle
+        }
+      }
+    }
+  }
+}
+  `;
+
+  const result = await fetch(
+    `${process.env.WORDPRESS_API_URL}?query=${encodeURIComponent(query)}`,
+    { headers: { "Content-Type": "application/json" } },
+  );
+  const data = await result.json();
+  const mini = data.data.page.pageBanners;
+  const homePrograms = data.data.programs.edges;
+
   return (
     <>
       <PageHero
         breadcrumbLabel="About Us"
         title={
-          <>
-            Building Africa&apos;s next
-            <br />
-            generation of <span className="text-gold-700">policy leaders</span>
-          </>
+          <div className="[&_span]:text-gold-700 [&_br]:hidden md:[&_br]:block" dangerouslySetInnerHTML={{ __html: mini.newPageTitle }} />
         }
       />
 
@@ -26,5 +139,5 @@ export default function AboutPage() {
 
       <RegisterCta />
     </>
-  )
+  );
 }
